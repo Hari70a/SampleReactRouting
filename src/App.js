19073ebbc16a442b757dd6d4dcc07a88a99d41c7
Hari70a <raja70a@gmail.com>
2018-12-17 13:10:55 +0630

    Routing addef

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+import {
+  BrowserRouter as Router,
+  Route,
+  NavLink,
+  Redirect
+} from "react-router-dom";
+
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+// import Error from "./components/Error";
+
+const User = props => {
+  console.log(props);
+  return <div>Welcome {props.name}</div>;
+};
 
 class App extends Component {
+  state = {
+    isLoggedIn: false,
+    username: ""
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <NavLink to="/" activeStyle={{ color: "green" }} exact>
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/about" activeStyle={{ color: "green" }} exact>
+                About
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact" activeStyle={{ color: "green" }} exact>
+                Contact
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/user" activeStyle={{ color: "green" }} exact>
+                User
+              </NavLink>
+            </li>
+          </ul>
+          <input
+            type="button"
+            value={this.state.isLoggedIn ? "Log out" : "Login"}
+            onClick={() =>
+              this.setState(prevState => {
+                return { isLoggedIn: !prevState.isLoggedIn };
+              })
+            }
+          />
+          <Route path="/" exact strict component={Home} />
+          <Route path="/about" exact strict component={About} />
+          <Route path="/contact" exact strict component={Contact} />
+          <Route
+            path="/user/:username"
+            exact
+            render={({ match }) =>
+              this.state.isLoggedIn ? (
+                <User name={match.params.username} />
+              ) : (
+                <Redirect to="/" />
+              )
+            }
+          />
+          {/* <Route component={Error} /> */}
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/Error.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Error extends Component {
+  render() {
+    return <div>This path doesn't exists</div>;
+  }
+}
+
+export default Error;
